09379449b785ad9e79ffb2efe32b4f06d6139af6
gerzarko <germanzarkovich@gmail.com>
2024-05-15 17:11:25 -0400

    Add first preview of new download implementation

--- src/components/common/cart/FreeDownloadButton.tsx
+++ src/components/common/cart/FreeDownloadButton.tsx
@@ -1,23 +1,87 @@
-import { onMount } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 import type { Component } from "solid-js";
 import type { Post } from "@lib/types";
 import { getLangFromUrl, useTranslations } from "@i18n/utils";
+import supabase from "@lib/supabaseClient";
+import type { AuthSession } from "@supabase/supabase-js";
+import type { Client } from "@lib/types";
 
 const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
 
 interface Props {
-    item: Post;
-    buttonClick: (event: Event) => void;
+  item: Post;
+  buttonClick: (event: Event) => void;
 }
 
-export const [items, setItems] = createStore<Post[]>([]);
 
+export const [items, setItems] = createStore<Post[]>([]);
+const { data: User, error: UserError } = await supabase.auth.getSession();
+
+async function postFormData(formData: FormData) {
+  const response = await fetch("/api/clientProfileEdit", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await response.json();
+  //Checks the API response for the redirect and sends them to the redirect page if there is one
+  if (data.redirect) {
+    alert(data.message);
+    window.location.href = `/${lang}` + data.redirect;
+  }
+  return data;
+}
 export const FreeDownloadButton: Component<Props> = (props: Props) => {
-    return (
-        <div class="relative z-10 w-full">
-            <button>{t("buttons.freeDownload")}</button>
-        </div>
-    );
+  const [client, setClient] = createSignal<Client>();
+  const [session, setSession] = createSignal<AuthSession | null>(null);
+
+  const fetchClient = async (user_id: string) => {
+    try {
+      const { data, error } = await supabase
+        .from("clientview")
+        .select("*")
+        .eq("user_id", user_id);
+
+      if (error) {
+        console.log(error);
+      } else if (data[0] === undefined) {
+        alert(t("messages.noClient")); //TODO: Change alert message
+        location.href = `/${lang}`;
+      } else {
+        console.log(data);
+        setClient(data[0]);
+        console.log(client());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  onMount(async () => {
+    setSession(User?.session);
+    await fetchClient(User?.session?.user.id!);
+    await getPurchasedItems();
+  });
+
+  const getPurchasedItems = async () => {
+    console.log("Session Info: ");
+    console.log(session());
+    const { data: orders, error } = await supabase
+      .from("orders")
+      .select("*")
+      .eq("customer_id", session()?.user.id);
+    console.log(orders, "no orders")
+    if (error) {
+      console.log("Orders Error: " + error.code + " " + error.message);
+      return;
+    }
+  }
+
+  return (
+    <div class="relative z-10 w-full">
+      <button onclick={() => console.log(1)}>{t("buttons.freeDownload")}</button>
+    </div>
+  );
 };
