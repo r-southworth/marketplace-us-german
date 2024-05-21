ad220142a13ea4ac7d89d217b694dd30c5655f15
gerzarko <germanzarkovich@gmail.com>
2024-05-21 12:25:55 -0400

    Redo download btn and api because I was doing it wrong I think

--- src/components/common/cart/FreeDownloadButton.tsx
+++ src/components/common/cart/FreeDownloadButton.tsx
@@ -11,77 +11,90 @@ const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
 
 interface Props {
-  item: Post;
-  buttonClick: (event: Event) => void;
+    item: Post;
+    buttonClick: (event: Event) => void;
 }
-
 
 export const [items, setItems] = createStore<Post[]>([]);
 const { data: User, error: UserError } = await supabase.auth.getSession();
 
-async function postFormData(formData: FormData) {
-  const response = await fetch("/api/clientProfileEdit", {
-    method: "POST",
-    body: formData,
-  });
-  const data = await response.json();
-  //Checks the API response for the redirect and sends them to the redirect page if there is one
-  if (data.redirect) {
-    alert(data.message);
-    window.location.href = `/${lang}` + data.redirect;
-  }
-  return data;
+// async function postFormData(formData: FormData) {
+//     const response = await fetch("/api/DownloadResources", {
+//         method: "POST",
+//         body: formData,
+//     });
+//     const data = await response.json();
+//     //Checks the API response for the redirect and sends them to the redirect page if there is one
+//     if (data.redirect) {
+//         alert(data.message);
+//         window.location.href = `/${lang}` + data.redirect;
+//     }
+//     return data;
+// }
+
+async function fetchClientCheckoutSecret() {
+    const response = await fetch("/api/createStripeCheckout", {
+        method: "POST",
+        body: JSON.stringify({
+            items: items,
+            userId: User.session?.user.id,
+            // orderId: orderId(),
+        }),
+    });
+    const { clientSecret } = await response.json();
+    return clientSecret;
 }
+
 export const FreeDownloadButton: Component<Props> = (props: Props) => {
-  const [client, setClient] = createSignal<Client>();
-  const [session, setSession] = createSignal<AuthSession | null>(null);
+    const [client, setClient] = createSignal<Client>();
+    const [session, setSession] = createSignal<AuthSession | null>(null);
 
-  const fetchClient = async (user_id: string) => {
-    try {
-      const { data, error } = await supabase
-        .from("clientview")
-        .select("*")
-        .eq("user_id", user_id);
+    console.log(props.item.id);
 
-      if (error) {
-        console.log(error);
-      } else if (data[0] === undefined) {
-        alert(t("messages.noClient")); //TODO: Change alert message
-        location.href = `/${lang}`;
-      } else {
-        console.log(data);
-        setClient(data[0]);
-        console.log(client());
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const fetchClient = async (user_id: string) => {
+        try {
+            const { data, error } = await supabase
+                .from("clientview")
+                .select("*")
+                .eq("user_id", user_id);
 
+            if (error) {
+                console.log(error);
+            } else if (data[0] === undefined) {
+                alert(t("messages.noClient")); //TODO: Change alert message
+                location.href = `/${lang}`;
+            } else {
+                console.log(data);
+                console.log(client());
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  onMount(async () => {
-    setSession(User?.session);
-    await fetchClient(User?.session?.user.id!);
-    await getPurchasedItems();
-  });
+    onMount(async () => {
+        setSession(User?.session);
+        await fetchClient(User?.session?.user.id!);
+        await getPurchasedItems();
+    });
 
-  const getPurchasedItems = async () => {
-    console.log("Session Info: ");
-    console.log(session());
-    const { data: orders, error } = await supabase
-      .from("orders")
-      .select("*")
-      .eq("customer_id", session()?.user.id);
-    console.log(orders, "no orders")
-    if (error) {
-      console.log("Orders Error: " + error.code + " " + error.message);
-      return;
-    }
-  }
+    const getPurchasedItems = async () => {
+        // console.log("Session Info: ");
+        // console.log(session());
+        const { data: orders, error } = await supabase
+            .from("orders")
+            .select("*")
+            .eq("customer_id", session()?.user.id);
+        console.log(orders, "no orders");
+        if (error) {
+            console.log("Orders Error: " + error.code + " " + error.message);
+            return;
+        }
+    };
 
-  return (
-    <div class="relative z-10 w-full">
-      <button onclick={() => console.log(1)}>{t("buttons.freeDownload")}</button>
-    </div>
-  );
+    return (
+        <div class="btn-primary relative z-10 w-full">
+            <button>{t("buttons.freeDownload")}</button>
+        </div>
+    );
 };
---
+++ src/pages/api/DownloadResources.tsx
@@ -0,0 +1,58 @@
+import supabase from "@lib/supabaseClientServiceRole";
+import type { APIRoute } from "astro";
+import { useTranslations } from "@i18n/utils";
+import { SITE } from "@src/config";
+import type { Post } from "@lib/types";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    const response = await request.json();
+    const signedUrl = response.signedUrl;
+    const user_id = response.userId;
+
+    // Validate the formData makes sure none of the fields are blank. Could probably do more than this like check for invalid phone numbers, blank strings, unselected location info etc.
+    if (!user_id) {
+        return new Response(
+            JSON.stringify({
+                //TODO Internationalize
+                message: "No User ID",
+            }),
+            { status: 400 }
+        );
+    }
+
+    // const { data, error } = await supabase.auth.admin.updateUserById(user_id, {
+    //     email: response.email,
+    //     email_confirm: true,
+    const { data, error } = await supabase.storage
+        .from("resources")
+        .createSignedUrl("private-document.pdf", 3600);
+
+    // if (data) {
+    //     console.log(data.signedUrl);
+    // } // });
+
+    if (error) {
+        return new Response(
+            JSON.stringify({
+                message: error.message,
+            }),
+            { status: 400 }
+        );
+    }
+
+    if (data.signedUrl !== null) {
+        // If everything works send a success response
+        return new Response(
+            JSON.stringify({
+                message: "Success",
+            }),
+            { status: 200 }
+        );
+    }
+    return new Response(
+        JSON.stringify({
+            message: "Failure",
+        }),
+        { status: 400 }
+    );
+};
