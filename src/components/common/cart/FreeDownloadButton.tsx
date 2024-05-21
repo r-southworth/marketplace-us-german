d4fc53f2ad389c6452dbf6bde03ea8d6efe9a1a9
gerzarko <germanzarkovich@gmail.com>
2024-05-21 12:38:44 -0400

    Move and re do function for orders

--- src/components/common/cart/FreeDownloadButton.tsx
+++ src/components/common/cart/FreeDownloadButton.tsx
@@ -18,38 +18,9 @@ interface Props {
 export const [items, setItems] = createStore<Post[]>([]);
 const { data: User, error: UserError } = await supabase.auth.getSession();
 
-// async function postFormData(formData: FormData) {
-//     const response = await fetch("/api/DownloadResources", {
-//         method: "POST",
-//         body: formData,
-//     });
-//     const data = await response.json();
-//     //Checks the API response for the redirect and sends them to the redirect page if there is one
-//     if (data.redirect) {
-//         alert(data.message);
-//         window.location.href = `/${lang}` + data.redirect;
-//     }
-//     return data;
-// }
-
-async function fetchClientCheckoutSecret() {
-    const response = await fetch("/api/createStripeCheckout", {
-        method: "POST",
-        body: JSON.stringify({
-            items: items,
-            userId: User.session?.user.id,
-            // orderId: orderId(),
-        }),
-    });
-    const { clientSecret } = await response.json();
-    return clientSecret;
-}
-
 export const FreeDownloadButton: Component<Props> = (props: Props) => {
     const [client, setClient] = createSignal<Client>();
     const [session, setSession] = createSignal<AuthSession | null>(null);
-
-    console.log(props.item.id);
 
     const fetchClient = async (user_id: string) => {
         try {
@@ -65,7 +36,6 @@ export const FreeDownloadButton: Component<Props> = (props: Props) => {
                 location.href = `/${lang}`;
             } else {
                 console.log(data);
-                console.log(client());
             }
         } catch (error) {
             console.log(error);
@@ -85,13 +55,24 @@ export const FreeDownloadButton: Component<Props> = (props: Props) => {
             .from("orders")
             .select("*")
             .eq("customer_id", session()?.user.id);
-        console.log(orders, "no orders");
         if (error) {
             console.log("Orders Error: " + error.code + " " + error.message);
             return;
         }
     };
 
+    async function fetchUrlFromBucket() {
+        const response = await fetch("/api/createStripeCheckout", {
+            method: "POST",
+            body: JSON.stringify({
+                items: items,
+                userId: User.session?.user.id,
+                orderId: orders(),
+            }),
+        });
+        const { clientSecret } = await response.json();
+        return clientSecret;
+    }
     return (
         <div class="btn-primary relative z-10 w-full">
             <button>{t("buttons.freeDownload")}</button>
